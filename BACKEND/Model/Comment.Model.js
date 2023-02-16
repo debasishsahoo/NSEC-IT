4845ade9c12841f1ee0d2ec22983d89427010b4b
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-02-16 12:03:28 +0530

    update Test Result

--- BACKEND/Model/Comment.Model.js
+++ BACKEND/Model/Comment.Model.js
@@ -11,7 +11,7 @@ const CommentSchema = mongoose.Schema({
     },
     date: {
         type: String,
-        required: true
+        // required: true
     },
     comments: {
         type: String,
--- BACKEND/Router/Comment.js
+++ BACKEND/Router/Comment.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 import { newComment, getComments, deleteComment } from '../Controller/Comment.Controller.js'
 
-router.post('/comment/new', newComment);
+router.post('/new', newComment);
 router.get('/comments/:id', getComments);
-router.delete('/comment/delete/:id', deleteComment);
+router.delete('/delete/:id', deleteComment);
 
 export default router;
