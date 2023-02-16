c0b9ec12499ba31af618ba3d2b5b42d786f26952
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-02-16 15:44:03 +0530

    Update on Sevice

---
+++ frontend/src/utils/Common-Utils.js
@@ -0,0 +1,28 @@
+export const getAccessToken = () => {
+    return sessionStorage.getItem('accessToken');
+}
+
+export const getRefreshToken = () => {
+    return sessionStorage.getItem('refreshToken');
+}
+
+export const setAccessToken = (accessToken) => {
+    return sessionStorage.setItem('accessToken', `Bearer ${accessToken}`);
+}
+
+export const setRefreshToken = (refreshToken) => {
+    return sessionStorage.setItem('refreshToken', `Bearer ${refreshToken}`);
+}
+
+export const getType = (value, body) => {
+    if (value.params) {
+        return { params: body }
+    } else if (value.query) {
+        if (typeof body === 'object') {
+            return { query: body._id }
+        } else {
+            return { query: body }
+        }
+    }
+    return {};
+}
